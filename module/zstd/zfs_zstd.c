ea67c0b3a4cabe377a410f2c6eff51c6226c4f54
Sebastian Gottschall <s.gottschall@dd-wrt.com>
2023-02-28 14:50:11 +0600

    merge zstd 1.5.4
    Signed-off-by: Sebastian Gottschall <s.gottschall@dd-wrt.com>

--- module/zstd/zfs_zstd.c
+++ module/zstd/zfs_zstd.c
@@ -34,6 +34,7 @@
  * Copyright (c) 2018-2020, Sebastian Gottschall
  * Copyright (c) 2019-2020, Michael Niewöhner
  * Copyright (c) 2020, The FreeBSD Foundation [1]
+ * Copyright (c) 2020, Mateusz Guzik
  *
  * [1] Portions of this software were developed by Allan Jude
  *     under sponsorship from the FreeBSD Foundation.
@@ -48,7 +49,7 @@
 
 #define	ZSTD_STATIC_LINKING_ONLY
 #include "lib/zstd.h"
-#include "lib/common/zstd_errors.h"
+#include "lib/zstd_errors.h"
 
 static uint_t zstd_earlyabort_pass = 1;
 static int zstd_cutoff_level = ZIO_ZSTD_LEVEL_3;
@@ -610,6 +611,7 @@ zfs_zstd_decompress_level_buf(void *s_start, void *d_start, size_t s_len,
 	uint32_t c_len;
 	const zfs_zstdhdr_t *hdr;
 	zfs_zstdhdr_t hdr_copy;
+	uint32_t version;
 
 	hdr = (const zfs_zstdhdr_t *)s_start;
 	c_len = BE_32(hdr->c_len);
@@ -626,15 +628,21 @@ zfs_zstd_decompress_level_buf(void *s_start, void *d_start, size_t s_len,
 	 * incompatibility occurs, it has to be handled accordingly.
 	 * The version can be accessed via `hdr_copy.version`.
 	 */
+	version = zfs_get_hdrversion(&hdr_copy);
 
 	/*
 	 * Convert and check the level
 	 * An invalid level is a strong indicator for data corruption! In such
 	 * case return an error so the upper layers can try to fix it.
 	 */
-	if (zstd_enum_to_level(curlevel, &zstd_level)) {
+	if (version >= 10405 &&
+	    zstd_enum_to_level(curlevel, &zstd_level)) {
 		ZSTDSTAT_BUMP(zstd_stat_dec_inval);
 		return (1);
+	}
+
+	if (version < 10405) {
+		zfs_set_hdrlevel(&hdr_copy, version);
 	}
 
 	ASSERT3U(d_len, >=, s_len);
@@ -652,8 +660,15 @@ zfs_zstd_decompress_level_buf(void *s_start, void *d_start, size_t s_len,
 		return (1);
 	}
 
-	/* Set header type to "magicless" */
-	ZSTD_DCtx_setParameter(dctx, ZSTD_d_format, ZSTD_f_zstd1_magicless);
+	/*
+	 * special case for supporting older development versions
+	 * which did contain the magic
+	 */
+	if (version >= 10405) {
+		/* Set header type to "magicless" */
+		ZSTD_DCtx_setParameter(dctx, ZSTD_d_format,
+		    ZSTD_f_zstd1_magicless);
+	}
 
 	/* Decompress the data and release the context */
 	result = ZSTD_decompressDCtx(dctx, d_start, d_len, hdr->data, c_len);
