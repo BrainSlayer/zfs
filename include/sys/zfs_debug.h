8b7db1e67be1ce592eed804b6938991851c8e46c
Sebastian Gottschall <s.gottschall@dd-wrt.com>
2025-04-23 13:00:48 +0700

    fix some functions
    Signed-off-by: Sebastian Gottschall <s.gottschall@dd-wrt.com>

--- include/sys/zfs_debug.h
+++ include/sys/zfs_debug.h
@@ -85,23 +85,11 @@ extern void __dprintf(boolean_t dprint, const char *file, const char *func,
  * use zfs_dbgmsg in high-frequency routines is the potential impact
  * that it can have on performance.
  */
-
-#ifdef ZFS_DEBUG
-#define	zfs_dbgmsg(...) \
-	if (zfs_dbgmsg_enable) \
-		__dprintf(B_FALSE, __FILE__, __func__, __LINE__, __VA_ARGS__)
 /*
  * To enable this:
  *
  * $ echo 1 >/sys/module/zfs/parameters/zfs_flags
  */
-#define	dprintf(...) \
-	if (zfs_flags & ZFS_DEBUG_DPRINTF) \
-		__dprintf(B_TRUE, __FILE__, __func__, __LINE__, __VA_ARGS__)
-#else
-#define	zfs_dbgmsg(...) do { } while (0)
-#define	dprintf(...) ((void)0)
-#endif /* ZFS_DEBUG */
 
 extern void zfs_panic_recover(const char *fmt, ...);
 
@@ -113,6 +101,7 @@ extern void zfs_dbgmsg_fini(void);
  * number and the text "nvlist <var name>:" followed by all the nvlist lines
  * without the file/fun/line number.  This makes the nvlist lines easy to read.
  */
+#ifdef ZFS_DEBUG
 #define	zfs_dbgmsg_nvlist(nv) \
 	if (zfs_dbgmsg_enable) { \
 		zfs_dbgmsg("nvlist "#nv":"); \
@@ -123,6 +112,14 @@ extern void zfs_dbgmsg_fini(void);
 	if (zfs_dbgmsg_enable) \
 		__dprintf(B_FALSE, __FILE__, __func__, __LINE__, __VA_ARGS__)
 
+#define	dprintf(...) \
+	if (zfs_flags & ZFS_DEBUG_DPRINTF) \
+		__dprintf(B_TRUE, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#else
+#define	zfs_dbgmsg(...) do { } while (0)
+#define	zfs_dbgmsg_nvlist(nv) do { } while (0)
+#define	dprintf(...) ((void)0)
+#endif /* ZFS_DEBUG */
 
 extern void __zfs_dbgmsg_nvlist(nvlist_t *nv);
 
